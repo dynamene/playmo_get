a26302bf0d6a1844564d4b0571a848b55aaf8aee
Kengo Wada <louiskengo3@gmail.com>
2020-06-08 21:23:42 +0300

    Added cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
+const axios = require('axios');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const { BASE_URL, getToken, validateLink } = require('./utils');
+
+exports.getPlaylistInfo = functions.https.onRequest(async (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(400).json({ message: 'Invalid request' });
+  }
+
+  const link = req.query.link;
+  const result = validateLink(link);
+  if (!result.isValid) {
+    return res.status(400).json({ message: 'Invalid link' });
+  }
+
+  const platform = result.platform;
+  const token = await getToken(platform);
+  const headers = { Authorization: `bearer ${token}` };
+  const url = `${BASE_URL}${platform}/?link=${link}`;
+
+  const response = await axios.get(url, { headers });
+  if (!response.data.isValid) {
+    return res.status(400).json({ message: 'Invalid link' });
+  }
+  return res.status(200).json(response.data.playlist);
+});
---
+++ functions/utils.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+
+const BASE_URL = 'https://us-central1-play-mo.cloudfunctions.net/';
+const PLATFORMS = [
+  { name: 'Deezer', link: 'deezer.com' },
+  { name: 'Tidal', link: 'tidal.com' },
+];
+
+async function getToken(funcName) {
+  const metadataServerURL =
+    'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=';
+  const url = `${metadataServerURL}${BASE_URL}${funcName}`;
+  const headers = { 'Metadata-Flavor': 'Google' };
+  const res = await axios.get(url, { headers });
+  return res.data;
+}
+
+function validateLink(link) {
+  if (!link) {
+    return { isValid: false };
+  }
+
+  let platformName = '';
+  for (let platform of PLATFORMS) {
+    if (link.includes(platform.link)) {
+      platformName = platform.name;
+      break;
+    }
+  }
+  if (!platformName) {
+    return { isValid: false };
+  }
+
+  switch (platformName) {
+    case 'Deezer': {
+      const regex = new RegExp('https://www.deezer.com/playlist/');
+      if (!regex.test(link)) {
+        return { isValid: false };
+      }
+      return { isValid: true, platform: 'deezer' };
+    }
+    case 'Tidal': {
+      const regex = new RegExp('https://tidal.com/browse/playlist/');
+      if (!regex.test(link)) {
+        return { isValid: false };
+      }
+      return { isValid: true, platform: 'tidal' };
+    }
+  }
+}
+
+module.exports = { BASE_URL, getToken, validateLink };
